641dcbbb731d9980ca0910785d1d2ce4c09b1305
Zubin Pratap <zubin.pratap@gmail.com>
2022-04-13 16:03:50 +1000

    rename token and refactor to move constants inside contracts, and initialize constructor inside contract.

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -5,10 +5,12 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-string constant _tokenName = "zubietoons";
-string constant _tokenSymbol = "Z-TOON";
+contract MusicNFTMarketplace is ERC721, Ownable {
+    string constant _tokenName = "zub-toon";
+    string constant _tokenSymbol = "Z-TOON";
 
-contract MusicNFTMarketplace is ERC721(_tokenName, _tokenSymbol), Ownable {
     string public baseURI =
         "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+
+    constructor() ERC721(_tokenName, _tokenSymbol) {}
 }
